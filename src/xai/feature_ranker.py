dbb8cc27720c049c1ef0661dc8a6f483f9c7dc9b
jaustrylle <sanren.reese@gmail.com>
2026-06-18 18:01:08 -0500

    Update feature_ranker.py
    Forgot the early stopping

--- src/xai/feature_ranker.py
+++ src/xai/feature_ranker.py
@@ -193,37 +193,3 @@
             
         logging.info(f"Saved explanations and plot for snapshot {snapshot_id} to {save_dir}")
         return explanation_data       
-
-class GNNEarlyStopping:
-    """Early stopping utility to stop training when 
-    macro F1 score across all anomaly classes does not improve.
-    We ignore the normal class (class 0) for early stopping as it is over-represented.    
-    """
-    def __init__(self, patience: int = 5, min_delta: float = 0.0001, mode: str = 'max'):
-        self.patience = patience
-        self.min_delta = min_delta
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.mode = mode
-    
-    def step(self, val: float):
-        if self.best_score is None:
-            self.best_score = val
-        elif self.mode == 'max' and val > self.best_score + self.min_delta:
-            self.best_score = val
-            self.counter = 0
-        elif self.mode == 'min' and val < self.best_score - self.min_delta:
-            self.best_score = val
-            self.counter = 0
-        # elif val == 0.0:
-        #     # special case to avoid early stopping at beginning
-        #     pass
-        else:
-            self.counter += 1
-            if self.counter >= self.patience:
-                self.early_stop = True
-                    
-        return self.early_stop
-
-# -------- Explanation functions --------
