a7561ac43dfc191e968e9a377307b6ee54d3c183
jaustrylle <sanren.reese@gmail.com>
2026-06-19 17:00:43 -0500

    Update ModelRunner.py
    Updated the interface

--- src/models/ModelRunner.py
+++ src/models/ModelRunner.py
@@ -1,33 +1,31 @@
 from abc import ABC, abstractmethod
+
 class ModelRunner(ABC):
-    """Abstract base class for running machine learning models with XAI support."""
 
     @abstractmethod
-    def train(self, data):
-        """Train the model with the provided data."""
+    def train(self, train_loader, val_loader):
         pass
 
     @abstractmethod
     def predict(self, data):
-        """Make predictions using the trained model."""
+        pass
+
+    @abstractmethod
+    def evaluate(self, test_loader):
         pass
 
     @abstractmethod
     def explain(self, data):
-        """Explain the model's predictions using XAI techniques."""
-        pass
-    
-    @abstractmethod
-    def save_model(self, path: str):
-        """Save the trained model to the specified path."""
         pass
 
     @abstractmethod
-    def load_model(self, path: str):
-        """Load a trained model from the specified path."""
+    def save_model(self, path):
         pass
 
     @abstractmethod
-    def evaluate(self, data, labels):
-        """Evaluate the model's performance on the provided data and labels."""
+    def load_model(self, path):
+        pass
+
+    @abstractmethod
+    def get_criterion(self, train_loader):
         pass
