a4cee960a7e5a11a3670d5e6ce16b5a0a38831db
jaustrylle <sanren.reese@gmail.com>
2026-06-19 16:46:19 -0500

    Create losses.py
    For class weights and calculations; reusability

---
+++ src/models/training/losses.py
@@ -0,0 +1,28 @@
+    def get_criterion(self, data_loader: DataLoader) -> nn.Module:
+        """Get loss function with class weights to handle class imbalance."""
+        
+        # Gather all labels from the dataset
+        labels = []
+        for batch in data_loader:
+            logging.info("Processing batch with %d graphs for criterion calculation.", batch.num_graphs)
+            logging.info("Batch labels: %s", batch.y.view(-1).long().cpu().numpy().tolist())
+            # Map labels to their corresponding anomaly classes, offset by -1 for CrossEntropyLoss
+            labels.extend(batch.y.view(-1).long().cpu().numpy().tolist())
+        
+        weights = self.get_weights(labels, min_num_classes=len(y_labels))
+        logging.info("Anomaly Classifier Class Weights: %s", weights)
+        anom_criterion = nn.CrossEntropyLoss(weight=torch.tensor(weights, dtype=torch.float, device=DEVICE))
+        self.criterion = anom_criterion
+        return anom_criterion
+
+        #['normal', 'anomaly', 'scan', 'dos', 'mitm', 'physical fault']
+        
+        # logging.info("Anomaly Classifier Class Weights: %s", weights)
+        # anom_criterion = FocalLoss(alpha=weights, gamma=2.0, reduction='mean')
+        # self.criterion = anom_criterion
+        # return anom_criterion
+
+
+# build class weights
+
+# build class entropy
