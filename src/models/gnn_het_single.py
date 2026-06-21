b003380234b492d29cb6c08fc6192b19c2a60b1b
jaustrylle <sanren.reese@gmail.com>
2026-06-20 21:14:02 -0500

    Update gnn_het_single.py
    evaluation moved near classification matrices

--- src/models/gnn_het_single.py
+++ src/models/gnn_het_single.py
@@ -66,27 +66,6 @@ torch.manual_seed(seed)
 np.random.seed(seed)
     
         
-    @torch.no_grad()
-    def evaluate_model(self, loader: DataLoader):
-        self.eval()
-        
-        total_loss = 0
-        total_num = 0
-        y_all = []
-        y_pred_all = []
-        for batch in loader:
-            batch = batch.to(DEVICE)
-            anom_logits = self(batch)      # [B,5]
-            y = batch.y.view(-1).long()
-            loss = self.criterion(anom_logits, y)
-            
-            total_loss += loss.item() * y.size(0)
-            total_num += y.size(0)
-            pred = anom_logits.argmax(dim=1)
-            y_all.extend(y.detach().cpu().tolist())
-            y_pred_all.extend(pred.detach().cpu().tolist())
-        mean_loss = total_loss / max(1, total_num)
-        return self.get_label_metrics(y_all, y_pred_all, mean_loss)
 
     @torch.no_grad()
     def predict(self, data: HeteroData) -> List[int]:
